8a36c8a015712c1f44f742e0ce6fdd78e4b64a76
Fredy <ronyramsol@gmail.com>
2026-04-09 12:34:53 -0600

    string y numeros

--- Sintaxis/string.py
+++ Sintaxis/string.py
@@ -25,3 +25,26 @@ espacios = "     este texto         "
 sinEspacios = espacios.strip()
 
 print(sinEspacios)
+
+
+#Slice, replace , Split
+
+#        0123456
+texto = "Este es un texto"
+
+print(texto[0:7])
+print(texto[7:])
+print(texto[0:-2])
+
+#Reemplazar
+curso = "Este curso es de JavaScript, JavaScript JavaScript"
+print (curso.replace("JavaScript", "Python"))
+
+#Split
+textoDividido = curso.split("J")
+print(textoDividido)
+
+#normalizar
+texto2 = "Este texto tiene MAYUSCULAS y minusculas y necesito encontrar ciertas palabras"
+
+print("Mayuscula".lower() in texto2.lower())
---
+++ conversionTipos.py
@@ -0,0 +1,24 @@
+x = 1
+y = 9.9
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+floatx = float(x)
+print(floatx)
+
+inty = int(y)
+print (inty)
+
+print(round(y,0))
+
+
+#numeros aleatorios
+import random
+
+print(random.randrange(1,10)) # rango 1 a 9
+
+
+
