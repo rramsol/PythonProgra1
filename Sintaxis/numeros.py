e1aad3c4de1fe737aa8a6ceb53472b6b9900fc6c
Fredy <ronyramsol@gmail.com>
2026-04-09 12:09:52 -0600

    numeros

---
+++ Sintaxis/numeros.py
@@ -0,0 +1,25 @@
+a = 1
+b = 3.1416
+c = 5 + 2j
+
+print(a)
+print(b)
+print(c)
+
+#listas
+lista = [0,1,2,3,4]
+
+#tuplas
+tupla = ("a","b","c")
+
+#Diccionario
+diccionario = {
+    "nombre": "juan",
+    "edad": 30,
+    "ciudad": "Xela"
+}
+
+#conjunto
+conjunto = {1,1,2,2,3} # {1,2,3}
+
+
